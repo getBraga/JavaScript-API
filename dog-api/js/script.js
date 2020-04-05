4a4e95969309c94b8f652c5026beef61a9d6d0c8
Ysmael Braga <ysmaelbraga01@gmail.com>
2020-04-04 21:37:03 -0300

    Foi feito o script da animação.

--- dog-api/js/script.js
+++ dog-api/js/script.js
@@ -8,48 +8,70 @@ const inputs = document.querySelectorAll('input');
 
 if(buttonImg && img && load && inputCep && inputs) {
 
-async function puxarDados() {
-  try {
-    const dogFetch = await fetch('https://dog.ceo/api/breeds/image/random');
-    const dog = await dogFetch.json();
-    load.classList.add('ativo');
-    img.classList.add('ativo');
-    setTimeout(() => {
-      load.classList.remove('ativo');
-      img.classList.remove('ativo');
-      img.src = dog.message;
-    }, 1500);
-  }
-  catch(erro) {
-    console.log(Error(erro));
-  }
-}
-puxarDados();
-
-
-inputCep.addEventListener('change', handleClick);
-
-buttonImg.addEventListener('click',puxarDados );
-
-function handleClick(event) {
-  event.preventDefault();
-  const cep = inputCep.value;
-  buscaCep(cep);
-}
-async function buscaCep(cep) {
- try{
-  const pegaURL = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-  const cepUrl = await pegaURL.json();
-      inputs[1].value =  cepUrl.logradouro;
-      inputs[3].value =  cepUrl.bairro;
-      inputs[4].value = cepUrl.localidade;
-      inputs[5].value = cepUrl.uf;
+  async function puxarDados() {
+    try {
+      const dogFetch = await fetch('https://dog.ceo/api/breeds/image/random');
+      const dog = await dogFetch.json();
+      load.classList.add('ativo');
+      img.classList.add('ativo');
+      setTimeout(() => {
+        load.classList.remove('ativo');
+        img.classList.remove('ativo');
+        img.src = dog.message;
+      }, 1500);
     }
     catch(erro) {
-       console.log(Error(erro));
-     alert('Cep Invalido');
-    
+      console.log(Error(erro));
     }
- 
-}
+  }
+  puxarDados();
+
+
+  inputCep.addEventListener('change', handleClick);
+
+  buttonImg.addEventListener('click',puxarDados );
+
+  function handleClick(event) {
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscaCep(cep);
+  }
+  async function buscaCep(cep) {
+  try{
+    const pegaURL = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const cepUrl = await pegaURL.json();
+        inputs[1].value =  cepUrl.logradouro;
+        inputs[3].value =  cepUrl.bairro;
+        inputs[4].value = cepUrl.localidade;
+        inputs[5].value = cepUrl.uf;
+      }
+      catch(erro) {
+        console.log(Error(erro));
+      alert('Cep Invalido');
+      
+      }
+  
+  }
+
+  function initAnimaScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+      const windowMetade = window.innerHeight * 0.75;
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = (sectionTop - windowMetade) < 0; 
+          if(isSectionVisible) {
+            section.classList.add('ativo');
+          }else{
+            section.classList.remove('ativo');
+          }
+  
+        })
+      } 
+      animaScroll()
+      window.addEventListener('scroll', animaScroll);
+    }
+  }
+  initAnimaScroll();
 }
